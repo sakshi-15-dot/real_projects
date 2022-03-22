9b7f4e22ed4abe3858b0ec2f2fc115842c39f2a9
sakshi-15-dot <69067198+sakshi-15-dot@users.noreply.github.com>
2022-03-22 16:38:35 +0530

    second commit

--- lms2/src/main/java/com/capg/lms2/LmsPortal2.java
+++ lms2/src/main/java/com/capg/lms2/LmsPortal2.java
@@ -4,6 +4,7 @@ public class LmsPortal2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//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
 		
 		System.out.println("This is java training");
 
